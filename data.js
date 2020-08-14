1eb7d89330caa024037fcfcb6f2cbb56104a0245
GustavoMatsunaga <63619508+GustavoMatsunaga@users.noreply.github.com>
2020-08-14 20:23:16 -0300

    Made a server with express | added a template engine with Nunjucks | Made some comments in all code | Refactored the code

---
+++ data.js
@@ -0,0 +1,77 @@
+module.exports = {
+    // Trailers to fill the trailers-page
+    trailers : [
+        {
+            id: "lfqHb6INj3w",
+            title: "The Da Vinci Code",
+            resume: `The Da Vinci Code is a 2003 mystery thriller novel by Dan Brown. It is Brown's second novel to include the character Robert Langdon: the first was his 2000 novel Angels & Demons. The Da Vinci Code follows "symbologist" Robert Langdon and cryptologist Sophie Neveu after a murder in the Louvre Museum in Paris causes them to become involved in a battle between the Priory of Sion and Opus Dei over the possibility of Jesus Christ and Mary Magdalene having had a child together.`,
+            date: "May 17",
+            year: "2006",
+            duration: "149 minutes",
+            director: "Ron Howard",
+            producer: "Brian Grazer and John Calley.",
+            featured: true
+        },
+        {
+            id: "AXCTMGYUg9A",
+            title: "Greatest Showman",
+            resume: `The Greatest Showman is a 2017 American musical biographical drama film directed by Michael Gracey in his directorial debut, written by Jenny Bicks and Bill Condon and starring Hugh Jackman, Zac Efron, Michelle Williams, Rebecca Ferguson, and Zendaya. Featuring nine original songs from Benj Pasek and Justin Paul, the film is inspired by the story of P. T. Barnum's creation of Barnum's American Museum and the lives of its star attractions.`,
+            date: "November 8",
+            year: "2017",
+            duration: "105 minutes",
+            director: "Michael Gracey",
+            producer: "Laurence Mark, Peter Chernin and Jenno Topping",
+        },
+        {
+            id: "GryRsVhOvxo",
+            title: "Bohemian Rhapsody",
+            resume: `Bohemian Rhapsody is a 2018 biographical drama film about Freddie Mercury, the lead singer of the British rock band Queen. It was directed by Bryan Singer[a] from a screenplay by Anthony McCarten, and produced by Graham King and Queen manager Jim Beach. It stars Rami Malek as Mercury, with Lucy Boynton, Gwilym Lee, Ben Hardy, Joe Mazzello, Aidan Gillen, Tom Hollander, Allen Leech, and Mike Myers in supporting roles. Queen members Brian May and Roger Taylor served as consultants. A British-American venture, the film was produced by 20th Century Fox, Regency Enterprises, GK Films, and Queen Films, with Fox serving as distributor. The film follows the singer's life from the formation of the band up to their 1985 Live Aid performance at the original Wembley Stadium.`,
+            date: "23 October",
+            year: "2018",
+            duration: "134 minutes",
+            director: "Bryan Singer",
+            producer: "Graham King and Jim Beach",
+            featured: true
+        },
+        {
+            id: "nuPZUUED5uk",
+            title: "The Imitation Game",
+            resume: `The Imitation Game is a 2014 historical drama film directed by Morten Tyldum and written by Graham Moore, based on the 1983 biography Alan Turing: The Enigma by Andrew Hodges. The title of the film quotes the name of the game Alan Turing proposed for answering the question "Can machines think?", in his 1950 seminal paper "Computing Machinery and Intelligence". It stars Benedict Cumberbatch as British cryptanalyst Alan Turing, who decrypted German intelligence messages for the British government during the Second World War. Keira Knightley, Matthew Goode, Rory Kinnear, Charles Dance, and Mark Strong also star.`,
+            date: "August 29",
+            year: "2014",
+            duration: "114 minutes",
+            director: "Morten Tyldum",
+            producer: "Nora Grossman, Ido Ostrowsky and Teddy Schwarzman",
+            featured: true
+        },
+        {
+            id: "tWAS-q2PpnA",
+            title: "X + Y",
+            resume: `The film, inspired by the documentary, Beautiful Young Minds, focuses on a teenage English mathematics prodigy named Nathan (Asa Butterfield) who has difficulty understanding people, but finds comfort in numbers. When he is chosen to represent the United Kingdom at the International Mathematical Olympiad (IMO), Nathan embarks on a journey in which he faces unexpected challenges, such as understanding the nature of love. The character of Nathan was based on Daniel Lightwing who won a silver medal at the 2006 IMO.`,
+            date: "September 5",
+            year: "2014",
+            duration: "111 minutes",
+            director: "Morgan Matthews",
+            producer: "David M. Thompson and Laura Hastings-Smith",
+            featured: true
+        },
+        {
+            id: "C6PbWhWGUrY",
+            title: "Moana",
+            resume: `The film tells the story of Moana, the strong-willed daughter of a chief of a Polynesian village, who is chosen by the ocean itself to reunite a mystical relic with the goddess Te Fiti. When a blight strikes her island, Moana sets sail in search of Maui, a legendary demigod, in the hope of returning the relic to Te Fiti and saving her people.`,
+            date: "November 14",
+            year: "2016",
+            duration: "107 minutes",
+            director: "Ron Clements and John Musker",
+            producer: "Osnat Shurer"
+        }
+    ],
+    // exporting socialMedia
+    socialMedia: {
+        linkedin: "https://www.linkedin.com/in/gustavo-matsunaga-0628461a3/",
+        github: "https://github.com/GustavoMatsunaga?tab=repositories",
+        stackOverFlow: "https://stackoverflow.com/users/14090106/gustavomatsunaga"
+    }
+}
+
+
